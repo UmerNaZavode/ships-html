75bc436210e55057424478c0e02986441c5e2706
gaba-linux-home <malkocmax@gmail.com>
2025-06-30 23:52:55 +0300

    vacancies section marking

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    '@typescript-eslint/recommended', // если используете TypeScript
+    'prettier', // должен быть последним
+  ],
+  parser: '@typescript-eslint/parser', // для TypeScript
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+  rules: {
+    'no-unused-vars': 'warn',
+    'no-console': 'warn',
+    'prefer-const': 'error',
+    'no-var': 'error',
+    'object-shorthand': 'error',
+    'prefer-arrow-callback': 'error',
+    'prefer-template': 'error',
+    'template-curly-spacing': 'error',
+    'arrow-spacing': 'error',
+    'comma-dangle': ['error', 'always-multiline'],
+    'semi': ['error', 'always'],
+    'quotes': ['error', 'single'],
+    'indent': ['error', 2],
+    'max-len': ['warn', { code: 100 }],
+  },
+  ignorePatterns: [
+    'dist/',
+    'build/',
+    'node_modules/',
+    '*.min.js',
+  ],
+};
