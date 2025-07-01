87a207238bf1db37e9290d8e9f6bc51402c61561
gaba-work <malkocmax@gmail.com>
2025-07-01 10:29:09 +0300

    one header for all pages

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,13 @@
 import './../scss/style.scss'
+
+document.addEventListener("DOMContentLoaded", function() {
+  // const header = document.queryselector
+  const header = document.querySelector(".header-alt")
+  console.log(header)
+
+  const current_url = window.location.pathname
+  if (current_url === "/") {
+    header.classList.add("header-alt--home")
+  }
+  console.log(current_url)
+});
